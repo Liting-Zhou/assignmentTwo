0bb7d50133965fb03c8c131ed3fbce961cb96afa
Liting-Zhou <143493320+Liting-Zhou@users.noreply.github.com>
2024-07-19 16:59:28 -0700

    change icons for the bottom tabs

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import { FontAwesome6 } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 import Activities from "./Activities";
 import Diet from "./Diet";
 import Settings from "./Settings";
@@ -10,9 +12,33 @@ const Tab = createBottomTabNavigator();
 export default function Home() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Activities" component={Activities} />
-      <Tab.Screen name="Diet" component={Diet} />
-      <Tab.Screen name="Settings" component={Settings} />
+      <Tab.Screen
+        name="Activities"
+        component={Activities}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="directions-run" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Diet"
+        component={Diet}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="fastfood" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
